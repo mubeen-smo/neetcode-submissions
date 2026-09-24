b3a8c12fe6a6c0834c7eaa3b1d21f74696dbd8f8
mubeen_s <mubeens.training@gmail.com>
2026-09-24 18:35:04 +0530

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') continue;
+                int boxIndex = 3*(i/3) + j/3;
+                int d = board[i][j] - '1';
+                if(rows[i][d] || cols[j][d] || boxes[boxIndex][d]) return false;
+                rows[i][d] = true;
+                cols[j][d] = true;
+                boxes[boxIndex][d] = true;
+            }
+        }
+
+        return true;
+    }
+}
+ 
